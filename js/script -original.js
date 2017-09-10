a9d0694eac7a2cece2a093af317519958b9106b1
kiyashko <30874362+kiyashko@users.noreply.github.com>
2017-09-10 23:40:26 +0300

    Джава
    Джава

---
+++ js/script -original.js
@@ -0,0 +1,23 @@
+var link = document.querySelector(".write-link");
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+ link.addEventListener("click", function (evt) {
+ evt.preventDefault();
+ popup.classList.add("modal-show");
+});
+ close.addEventListener("click", function (evt) {
+ evt.preventDefault();
+ popup.classList.remove("modal-show");
+});
+
+var link = document.querySelector(".write-link");
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+ link.addEventListener("click", function (evt) {
+ evt.preventDefault();
+ popup.classList.add("modal-show");
+});
+ close.addEventListener("click", function (evt) {
+ evt.preventDefault();
+ popup.classList.remove("modal-show");
+});
