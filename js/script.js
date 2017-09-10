0b08c3e3a0442180d5ddf0b39cf4bfc4f9eef459
kiyashko <30874362+kiyashko@users.noreply.github.com>
2017-09-11 00:01:54 +0300

    Финал
    Финал

--- js/script.js
+++ js/script.js
@@ -10,14 +10,3 @@ var close = popup.querySelector(".modal-close");
  popup.classList.remove("modal-show");
 });
 
-var link = document.querySelector(".write-link");
-var popup = document.querySelector(".modal-write-us");
-var close = popup.querySelector(".modal-close");
- link.addEventListener("click", function (evt) {
- evt.preventDefault();
- popup.classList.add("modal-show");
-});
- close.addEventListener("click", function (evt) {
- evt.preventDefault();
- popup.classList.remove("modal-show");
-});
